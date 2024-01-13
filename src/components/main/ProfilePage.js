478bca39d7371109ac02c5567b593bdf6f352819
Gideon <60163920+jgidoD@users.noreply.github.com>
2024-01-13 14:20:24 +0800

    Add files via upload

--- src/components/main/ProfilePage.js
+++ src/components/main/ProfilePage.js
@@ -1,14 +1,52 @@
+import "./ProfilePage.css";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams, Link } from "react-router-dom";
 import { collection, getDocs, doc, query, where } from "firebase/firestore";
-import { Box, Heading, useCardStyles } from "@chakra-ui/react";
-import { db } from "../../firebase/firebaseConfig";
+import {
+  Box,
+  Heading,
+  useCardStyles,
+  Card,
+  Flex,
+  Text,
+  MenuButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  IconButton,
+  Image,
+  MenuGroup,
+  MenuDivider,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalHeader,
+  ModalCloseButton,
+  ModalContent,
+} from "@chakra-ui/react";
+import { db, auth } from "../../firebase/firebaseConfig";
 import { UserAuth } from "../context/AuthContext";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { signOut } from "firebase/auth";
+import { formatDistanceToNow } from "date-fns";
+import Profile from "./Profile";
+import PostOptions from "./mainComponents/PostOptions";
+import { Home, Compass, User, LogOut, Edit } from "react-feather";
+import Comment from "./mainComponents/Comment";
 
 function ProfilePage() {
   const { userId } = useParams();
+  // const { postId } = useParams();
   const { user } = UserAuth();
   const [userData, setUserData] = useState(null);
+  const [postData, setPostData] = useState(null);
+  const navigate = useNavigate();
+  const editProfile = useDisclosure();
+
+  const handleSignOut = () => {
+    signOut(auth);
+    navigate("/");
+  };
 
   //check the location
   let getUrl = window.location.href;
@@ -17,10 +55,6 @@ function ProfilePage() {
 
   const loadData = async () => {
     if (!userId) return; // Handle missing ID
-
-    // const docRef = doc(db, "users1", splitUrl[4]); // Replace "db" with  Firestore instance
-    // const userDataVar = await getDocs(docRef);
-    // console.log(userDataVar.data());
 
     // const docRef = collection(db, "users1"); // Replace "db" with  Firestore instance
     // const docSnap = query(docRef, where("userID", "==", splitUrl[4]));
@@ -36,44 +70,234 @@ function ProfilePage() {
       tempArr.push(doc.data());
     });
     setUserData(...tempArr);
-    console.log(testData);
-    // const q = query(
-    //   collection(db, "users1"),
-    //   where("userID", "==", splitUrl[4])
-    // );
-    // const querySnapshot = await getDocs(q);
-    // querySnapshot.forEach((doc) => {
-    //   console.log(doc.id, " => ", doc.data());
-    // });
-
-    // let sanitizeUserData = db
-    //   .collection("users1")
-    //   .where("userID", splitUrl[4])
-    //   .get();
-    // console.log(sanitizeUserData);
-
-    // const docRef = collection(db, "users1"); // Replace "db" with  Firestore instance
-    // const docSnap = query(docRef, where("userID", "==", splitUrl[4]));
-    // const userDataVar = await getDocs(docSnap);
-    // console.log(userDataVar);
   };
 
   useEffect(() => {
     loadData();
   }, [userId]);
 
+  useEffect(() => {
+    const getUserPosts = async () => {
+      const docRef = collection(db, "posts");
+      const docSnap = query(docRef, where("authorId", "==", userId));
+      const postDataVar = await getDocs(docSnap);
+
+      let tempArr = [];
+
+      let testData = postDataVar.forEach((doc) => {
+        tempArr.push({ ...doc.data(), id: doc.id });
+      });
+      setPostData(tempArr);
+    };
+    getUserPosts();
+  }, []);
+
   return (
-    <Box>
-      {userData ? (
-        <Box>
-          <Heading>{userData.name}'s Profile</Heading>
-          {/* ... display other user's information */}
-          {userData.userID}
-        </Box>
-      ) : (
-        <Box>Loading user profile...</Box>
-      )}
-    </Box>
+    <>
+      <Box>
+        {userData && userData ? (
+          <Box key={userData.id}>
+            <Box>
+              <Flex
+                className="navbar"
+                justify="space-between"
+                px="32px"
+                py="12px"
+                boxShadow="1px 0px 12px #aeaeae"
+                w="100vw"
+                overflow="hidden"
+              >
+                <Heading size="xl">Feed</Heading>
+                <Flex>
+                  <Menu>
+                    <MenuButton
+                      as={IconButton}
+                      variant="outline"
+                      icon={<HamburgerIcon />}
+                    ></MenuButton>
+                    <MenuList>
+                      <MenuItem
+                        onClick={() => {
+                          navigate("/dashboard");
+                        }}
+                        icon={<Home size={16} />}
+                      >
+                        Home
+                      </MenuItem>
+                      <Link to="/discover">
+                        <MenuItem icon={<Compass size={16} />}>
+                          Discover
+                        </MenuItem>
+                      </Link>
+
+                      <MenuDivider />
+                      <MenuGroup title="Account">
+                        <MenuItem
+                          onClick={() => {
+                            navigate(`/profile/${user.uid}`);
+                          }}
+                          icon={<User size={16} />}
+                        >
+                          Profile
+                        </MenuItem>
+                        <MenuItem
+                          icon={<LogOut size={16} />}
+                          onClick={handleSignOut}
+                        >
+                          Logout
+                        </MenuItem>
+                      </MenuGroup>
+                    </MenuList>
+                  </Menu>
+                </Flex>
+              </Flex>
+            </Box>
+
+            {/* ... display other user's information */}
+            <Flex
+              pt="24px"
+              align="center"
+              justify="center"
+              flexDirection="column"
+            >
+              <Flex w="100%">
+                <Box>
+                  <Flex
+                    justify="center"
+                    align="center"
+                    h="150px"
+                    w="150px"
+                    ml="200px"
+                    borderRadius="50%"
+                    border="1px solid red"
+                  >
+                    <Text>Profile here</Text>
+                  </Flex>
+                </Box>
+
+                <Flex mx="100px" flexDirection="column">
+                  <Box>
+                    <Heading>{userData.name}</Heading>
+                    <Text fontSize="sm">
+                      <strong>UID: </strong>
+                      {userData.userID}
+                    </Text>
+                    <Text color="#9c9c9c" fontSize="xs" as="i">
+                      Member since {formatDistanceToNow(userData.dateCreated)}{" "}
+                      ago
+                    </Text>
+                  </Box>
+
+                  <br />
+
+                  <Box>
+                    <Text>
+                      <strong>Location: </strong>
+                      {userData.location}
+                    </Text>
+                    <Text>
+                      <strong>Email: </strong>
+                      {userData.email}
+                    </Text>
+                    <Text>
+                      <strong>Phone Number: </strong>
+                      {userData.phone}
+                    </Text>
+                  </Box>
+                </Flex>
+                <Box>
+                  <Button
+                    onClick={() => {
+                      editProfile.onOpen();
+                    }}
+                    variant="outline"
+                  >
+                    Edit Profile
+                  </Button>
+                  <Modal
+                    isOpen={editProfile.isOpen}
+                    onCLose={editProfile.onClose}
+                  >
+                    <ModalHeader>Profile</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalContent>HATDOG</ModalContent>
+                  </Modal>
+                </Box>
+              </Flex>
+
+              <Flex
+                w="100%"
+                justify="center"
+                align="center"
+                flexDirection="column"
+                boxShadow="0px -4px 5px #e1e1e1"
+                mt="32px"
+                py="24px"
+              >
+                {postData && postData.length === 0 ? (
+                  <Flex justify="center" align="center">
+                    <Text color="#7f7f7f">It feels so lonely here...</Text>
+                  </Flex>
+                ) : (
+                  postData &&
+                  postData.map((post) => (
+                    <Card key={post.id} w="50%" p="24px 24px" my="12px">
+                      <Flex flexDirection="column">
+                        <Box>
+                          <Profile name={post.name} authorId={post.authorId} />
+                        </Box>
+                        <PostOptions
+                          postId={post.id}
+                          authorId={post.authorId}
+                        />
+                        <Text as="kbd" fontSize="10px" color="gray.500">
+                          {formatDistanceToNow(post.datePosted)} ago
+                        </Text>
+
+                        <Flex pl="32px" py="32px" justify="space-between">
+                          <Box>
+                            <Heading size="md">{post.postTitle}</Heading>
+                            <br />
+
+                            <Text fontSize="16px">{post.postContent}</Text>
+                          </Box>
+
+                          <Box mr="24px">
+                            {!post.price ? (
+                              <Text>₱ 0.00</Text>
+                            ) : (
+                              <>
+                                <strong>₱ </strong>
+                                {post.price}
+                              </>
+                            )}
+                          </Box>
+                        </Flex>
+                        <Flex w="100%" align="center" justify="center">
+                          <Image
+                            src={post.postImg}
+                            w="20em"
+                            alt="post image"
+                            onError={(e) => (e.target.style.display = "none")}
+                          />
+                        </Flex>
+                        <Box w="100%">
+                          <Comment postID={post.id} authorId={post.authorId} />
+                        </Box>
+                      </Flex>
+                    </Card>
+                  ))
+                )}
+              </Flex>
+            </Flex>
+          </Box>
+        ) : (
+          <Flex w="100%" h="100vh" align="center" justify="center">
+            <span className="loader"></span>
+          </Flex>
+        )}
+      </Box>
+    </>
   );
 }
 
--- src/components/main/mainComponents/PostOptions.js
+++ src/components/main/mainComponents/PostOptions.js
@@ -13,28 +13,48 @@ import {
   AlertDialogContent,
   AlertDialogFooter,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
-import { doc, getDoc, collection, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  collection,
+  deleteDoc,
+  getDocs,
+} from "firebase/firestore";
 import { auth, db } from "../../../firebase/firebaseConfig";
 import { UserAuth } from "../../context/AuthContext";
-import { useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import ShareModal from "./postOptionsComponents/ShareModal";
+import { useNavigate } from "react-router-dom";
 const PostOptions = (props) => {
   const { user } = UserAuth();
 
   const postId = props.postId;
   const fetchData = props.fetchData;
   const authorId = props.authorId;
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const deleteOverlay = useDisclosure();
+  const shareModal = useDisclosure();
   const cancelRef = useRef();
-
+  const [postContent, setPostContent] = useState();
+  const navigate = useNavigate();
+  const getPost = async () => {
+    const colRef = collection(db, "posts", postId);
+    const docSnap = await getDoc(colRef);
+    console.log(docSnap);
+  };
   const handleDelete = async () => {
     try {
       const postRef = doc(db, "posts", postId);
-      const commentRef = collection(postRef, "comments");
+      // const commentRef = collection(postRef, "comments");
       await deleteDoc(postRef);
     } catch (err) {
       console.log(err.message);
@@ -42,7 +62,10 @@ const PostOptions = (props) => {
 
     fetchData();
   };
-
+  const handleShareButton = () => {
+    // shareModal.onOpen();
+    navigate(`/profile/${authorId}/post/${postId}`);
+  };
   return (
     <>
       <Menu>
@@ -58,18 +81,34 @@ const PostOptions = (props) => {
         <MenuList>
           <MenuItem
             onClick={() => {
-              onOpen();
+              deleteOverlay.onOpen();
             }}
             isDisabled={authorId !== user.uid}
           >
             Delete
           </MenuItem>
+          <MenuItem onClick={handleShareButton}>
+            Share
+            {/* modal for sharePost */}
+            <Modal isOpen={shareModal.isOpen} onClose={shareModal.onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Share your post.</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody></ModalBody>
+
+                <ModalFooter>
+                  <Button w="100%">Copy Link </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </MenuItem>
         </MenuList>
       </Menu>
       <AlertDialog
-        isOpen={isOpen}
+        isOpen={deleteOverlay.isOpen}
         leastDestructiveRef={cancelRef}
-        onClose={onClose}
+        onClose={deleteOverlay.onClose}
       >
         <AlertDialogOverlay>
           <AlertDialogContent>
@@ -85,14 +124,14 @@ const PostOptions = (props) => {
               <Button
                 colorScheme="red"
                 onClick={() => {
-                  onClose();
+                  deleteOverlay.onClose();
                   handleDelete();
                 }}
                 ml={3}
               >
                 Delete
               </Button>
-              <Button ref={cancelRef} onClick={onClose}>
+              <Button ref={cancelRef} onClick={deleteOverlay.onClose}>
                 Cancel
               </Button>
             </AlertDialogFooter>
